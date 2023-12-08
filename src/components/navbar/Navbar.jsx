39db2f138efb65a22d8adc463a5152ac525cce8f
Arsen Hendricks <arsenhendricks@gmail.com>
2023-12-08 19:33:20 +0300

    Changed the Navbar names

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -17,16 +17,16 @@ const Navbar = () => {
             <a href="#home"> Home </a>{" "}
           </p>
           <p>
-            <a href="#wgpt3"> What is GPT </a>{" "}
+            <a href="#wgpt3"> About </a>{" "}
           </p>
           <p>
-            <a href="#possibility"> Open AI </a>{" "}
+            <a href="#possibility"> Services </a>{" "}
           </p>
           <p>
-            <a href="#features"> Case Studies </a>{" "}
+            <a href="#features"> Features </a>{" "}
           </p>
           <p>
-            <a href="#blog"> Library </a>{" "}
+            <a href="#blog"> Blog </a>{" "}
           </p>
         </div>
       </div>
