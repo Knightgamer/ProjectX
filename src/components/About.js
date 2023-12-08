70a7ee8287c13ad60c93765fe4e11dec69e57e35
Arsen Hendricks <arsenhendricks@gmail.com>
2023-12-08 22:14:22 +0300

    Made the UI more user friendly

--- src/components/About.js
+++ src/components/About.js
@@ -1,39 +1,47 @@
 import React from "react";
-import projectx from "../assets/projectx.jpg"; 
+import projectx from "../assets/projectx.jpg";
 
 const About = () => {
   return (
-    <section id="about" className="bg-gray-100 p-8 md:p-12 lg:p-16">
-      <div className="container mx-auto flex flex-wrap justify-center items-center">
-        <div className="md:flex-1 flex justify-center mb-6 md:mb-0 p-4">
-          <img
-            src={projectx}
-            alt="Team celebrating success"
-            className="max-w-xs md:max-w-md lg:max-w-lg h-auto rounded-lg shadow-lg transform transition duration-500 hover:scale-105"
-          />
-        </div>
-        <div className="md:flex-1 text-lg text-gray-700 p-4">
-          <h2 className="text-3xl md:text-4xl font-bold text-center mb-6 md:mb-10">
-            About Us
-          </h2>
-          <p>
-            ProjectX is a cutting-edge web platform designed to bring{" "}
-            <strong>innovative solutions</strong> to our users. We focus on
-            creating user-friendly, responsive, and dynamic applications that
-            cater to a diverse range of needs.
-          </p>
-          <p className="mt-4">
-            Our team comprises dedicated professionals with expertise in various
-            fields, including web development, design, and user experience. We
-            believe in the power of technology to transform and improve people's
-            lives.
-          </p>
-          <p className="mt-4">
-            Since our inception in [Year], we have been committed to excellence,
-            innovation, and customer satisfaction. Join us on our journey as we
-            continue to explore new horizons and push the boundaries of what's
-            possible on the web.
-          </p>
+    <section id="about" className="bg-gray-100">
+      <div className="container mx-auto px-4 py-12 md:py-24">
+        <div className="grid grid-cols-1 md:grid-cols-12 gap-8 items-center">
+          <div className="md:col-span-5 lg:col-span-4">
+            <img
+              src={projectx}
+              alt="About ProjectX"
+              className="w-full rounded-lg shadow-lg"
+            />
+          </div>
+          <div className="md:col-span-7 lg:col-span-8">
+            <h2 className="text-3xl md:text-4xl font-bold text-gray-800 mb-6">
+              About Us
+            </h2>
+            <p className="text-lg text-gray-700 mb-4">
+              ProjectX is a cutting-edge web platform designed to bring{" "}
+              <strong>innovative solutions</strong> to our users. We focus on
+              creating user-friendly, responsive, and dynamic applications that
+              cater to a diverse range of needs.
+            </p>
+            <p className="text-lg text-gray-700 mb-4">
+              Our team comprises dedicated professionals with expertise in
+              various fields, including web development, design, and user
+              experience. We believe in the power of technology to transform and
+              improve people's lives.
+            </p>
+            <p className="text-lg text-gray-700 mb-8">
+              Since our inception in [Year], we have been committed to
+              excellence, innovation, and customer satisfaction. Join us on our
+              journey as we continue to explore new horizons and push the
+              boundaries of what's possible on the web.
+            </p>
+            <a
+              href="#contact"
+              className="inline-block bg-blue-500 text-white px-6 py-3 rounded-lg shadow-lg hover:bg-blue-600 transition duration-300"
+            >
+              Get in Touch
+            </a>
+          </div>
         </div>
       </div>
     </section>
